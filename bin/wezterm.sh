08ccf271addea8d309ed8a68ad0094d3adf7133b
hyouhyan <hyouhyan@hyouhyan.com>
2025-10-11 09:51:24 +0900

    feat: initスクリプト書いた

---
+++ bin/wezterm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/.config
+
+ln -s ${SCRIPTPATH}/wezterm ~/.config/wezterm
---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# env
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )/bin"
+
+# initialize something
+${SCRIPTPATH}/wezterm.sh
