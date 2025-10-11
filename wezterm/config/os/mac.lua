38f38c5da465a0d671467c887ed703c8c0c4129e
hyouhyan <hyouhyan@hyouhyan.com>
2025-10-11 18:13:21 +0900

    feat: [wexterm] macでは最初からフルスクリーンで表示するようにした

--- wezterm/config/os/mac.lua
+++ wezterm/config/os/mac.lua
@@ -2,4 +2,11 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
+-- 最初からフルスクリーンで起動
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
+
 return config
