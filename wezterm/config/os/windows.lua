fb2ec3ce82b1af5d2c1620873889f5fd746cdafa
hyouhyan <hyouhyan@hyouhyan.com>
2025-12-30 01:52:24 +0900

    feat: windowsのweztermを最初からフルスクリーンにした

--- wezterm/config/os/windows.lua
+++ wezterm/config/os/windows.lua
@@ -12,6 +12,13 @@ end
 -- デフォルトをWSLのByobuにする
 config.default_prog = { 'wsl.exe', '--cd', '~', "-e", "byobu" }
 
+-- 最初からフルスクリーンで起動
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
+
 -- Launch Menu
 merge_config(config, require("config/os/launch_menu/windows"))
 
